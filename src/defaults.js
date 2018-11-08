4f005016bc32b9e4c3b4a88397e5773cbf3d94c8
Dennis Sen <dennis.sen@dataport.de>
2018-11-08 15:12:51 +0100

    add mapView.js and defaults.js file
    MPAPI-6

---
+++ src/defaults.js
@@ -0,0 +1,22 @@
+export default {
+    target: "map",
+    epsg: "EPSG:25832",
+    backgroundImage: "https://geoportal-hamburg.de/lgv-config/img/backgroundCanvas.jpeg",
+    extent: [510000.0, 5850000.0, 625000.4, 6000000.0],
+    options: [
+        {resolution: 66.14579761460263, scale: 250000, zoomLevel: 0},
+        {resolution: 26.458319045841044, scale: 100000, zoomLevel: 1},
+        {resolution: 15.874991427504629, scale: 60000, zoomLevel: 2},
+        {resolution: 10.583327618336419, scale: 40000, zoomLevel: 3},
+        {resolution: 5.2916638091682096, scale: 20000, zoomLevel: 4},
+        {resolution: 2.6458319045841048, scale: 10000, zoomLevel: 5},
+        {resolution: 1.3229159522920524, scale: 5000, zoomLevel: 6},
+        {resolution: 0.6614579761460262, scale: 2500, zoomLevel: 7},
+        {resolution: 0.2645831904584105, scale: 1000, zoomLevel: 8},
+        {resolution: 0.13229159522920521, scale: 500, zoomLevel: 9}
+    ],
+    startResolution: 15.874991427504629,
+    startCenter: [565874, 5934140],
+    layerConf: "http://geoportal-hamburg.de/lgv-config/services-internet.json",
+    units: "m"
+};
---
+++ src/mapView.js
@@ -0,0 +1,21 @@
+import _ from "underscore";
+import {View} from "ol";
+import defaults from "./defaults";
+
+/**
+ * Creates a View from config.
+ * @param {object} config - configuration object
+ * @returns {ol/View} view with start values from config
+ */
+export function createMapView (config) {
+    var options = _.extend({}, defaults, config),
+        viewParams = {
+            projection: options.epsg,
+            center: options.startCenter,
+            extent: options.extent,
+            resolution: options.startResolution,
+            resolutions: options.options.map(entry => entry.resolution)
+        };
+
+    return new View(viewParams);
+}
