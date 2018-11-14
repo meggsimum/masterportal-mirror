e422af1c36a389d216df52661423fa7e8edef7fa
Dennis Sen <dennis.sen@dataport.de>
2018-11-14 14:55:51 +0100

    refactor make test more readable

--- test/layer/lib.test.js
+++ test/layer/lib.test.js
@@ -18,43 +18,62 @@ describe("wms.js", function () {
     });
 
     describe("getLegendURLs", function () {
-        const definedURL = {legendURL: "example.com/legend.png", layers: "a,b", url: "example.com", version: "0.0.1", typ: "WMS"},
-            ignoreLegend = {legendURL: "ignore", layers: "a,b", url: "example.com", version: "0.0.1", typ: "WMS"},
-            noLayer = {layers: "", url: "example.com", version: "0.0.1", typ: "WMS"},
-            oneLayer = {layers: "a", url: "example.com", version: "0.0.1", typ: "WMS"},
-            twoLayers = {layers: "a,b", url: "example.com", version: "0.0.1", typ: "WMS"},
-            expectedBaseString = "example.com&SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png?VERSION=0.0.1&LAYER=";
+        const expectedBaseString = "example.com&SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png?VERSION=0.0.1&LAYER=";
 
         it("returns the defined legendURL if given via services.json", function () {
-            const legendURL = lib.getLegendURLs(definedURL);
+            const legendURL = lib.getLegendURLs({
+                legendURL: "example.com/legend.png",
+                layers: "a,b",
+                url: "example.com",
+                version: "0.0.1",
+                typ: "WMS"
+            });
 
             expect(legendURL).toEqual(["example.com/legend.png"]);
         });
 
         it("returns an empty array if legendURL is defined as 'ignore' services.json", function () {
-            const legendURL = lib.getLegendURLs(ignoreLegend);
+            const legendURL = lib.getLegendURLs({
+                legendURL: "ignore",
+                layers: "a,b",
+                url: "example.com",
+                version: "0.0.1",
+                typ: "WMS"
+            });
 
             expect(legendURL).toEqual([]);
         });
 
-        /*
-        example.com&SERVICE=WMS&REQUEST=GetLegendGraphic&FORMAT=image/png?VERSION=0.0.1&LAYER=a
-        */
         it("returns an empty array if no layers were requested", function () {
-            const legendURLs = lib.getLegendURLs(noLayer);
+            const legendURLs = lib.getLegendURLs({
+                layers: "",
+                url: "example.com",
+                version: "0.0.1",
+                typ: "WMS"
+            });
 
             expect(legendURLs.length).toBe(0);
         });
 
         it("returns one correct legend URL if one layer was requested", function () {
-            const legendURLs = lib.getLegendURLs(oneLayer);
+            const legendURLs = lib.getLegendURLs({
+                layers: "a",
+                url: "example.com",
+                version: "0.0.1",
+                typ: "WMS"
+            });
 
             expect(legendURLs.length).toBe(1);
             expect(legendURLs[0]).toEqual(expectedBaseString + "a");
         });
 
         it("returns multiple correct legend URLs if multiple layers were requested", function () {
-            const legendURLs = lib.getLegendURLs(twoLayers);
+            const legendURLs = lib.getLegendURLs({
+                layers: "a,b",
+                url: "example.com",
+                version: "0.0.1",
+                typ: "WMS"
+            });
 
             expect(legendURLs.length).toBe(2);
             expect(legendURLs[0]).toEqual(expectedBaseString + "a");
