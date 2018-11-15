4cfe29e5f3e47d765aedd3d5fa090aa08cf15f3d
Dennis Sen <dennis.sen@dataport.de>
2018-11-15 09:21:33 +0100

    fix remove findWhere from jsdoc

--- src/lib/findWhere.js
+++ src/lib/findWhere.js
@@ -3,6 +3,7 @@
  * @param {Array} array - array to return first element meeting condition of
  * @param {function} condition - boolean function
  * @returns {*} first array entry that meets condition, else null
+ * @ignore
  */
 export default function (array, condition) {
     for (let i = 0; i < array.length; i++) {
