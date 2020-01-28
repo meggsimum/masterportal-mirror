9fc97220bae64d8ac942145042370707aa187815
Dudek <DudekIn@fhhnet.stadt.hamburg.de>
2020-01-28 14:35:10 +0100

    revert: addLAyer is not exportetd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {createMap, addLayer} from "./map";
+import {createMap} from "./map";
 import {createMapView} from "./mapView";
 import * as crs from "./crs";
 import * as rawLayerList from "./rawLayerList";
@@ -10,7 +10,6 @@ import setBackgroundImage from "./lib/setBackgroundImage";
 
 export {
     createMap,
-    addLayer,
     createMapView,
     wms,
     geojson,
