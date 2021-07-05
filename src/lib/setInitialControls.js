03d376c5efea6fa8ec3216c0ae4e464a72519373
richard_schmid <richard.schmid@gv.hamburg.de>
2021-07-05 14:29:32 +0200

    added lazyLoading of Cesium

--- src/lib/setInitialControls.js
+++ src/lib/setInitialControls.js
@@ -1,6 +1,27 @@
 import abstractAPI from "../../abstraction/api.js";
 
 /**
+ * Loads the specified JS and calls the given callback after the file has loaded. Modifies the DOM for loading.
+ * @param {String} fileName - The URL to Load
+ * @param {Function} callback - the Function to call after loading has completed
+ * @returns {undefined}
+ */
+ var loadScript = function(fileName, callback) {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = fileName;
+
+    script.onload = callback;
+    script.onreadystatechange = function() {
+        if(this.readyState == 'complete') {
+            callback();
+        }
+    };
+    head.appendChild(script);
+};
+
+/**
  * Returns map controls according to config.controls given in the config.json.
  * @param {object} config - configuration object
  * @returns {undefined}
@@ -14,18 +35,21 @@ export default function (config) {
                 settings3D = {
                     "map2D": window.mpapi.map
                 },
-                map3D = abstractAPI.map.createMap(settings3D, "3D"),
                 // eslint-disable-next-line func-style
                 setEnabled = function () {
-                    if (window.mpapi.map === map3D) {
+                    if (window.mpapi.map.mapMode === "3D") {
                         window.mpapi.map.setEnabled(false);
                         window.mpapi.map = map2D;
                         switch3D.value = "3D";
                     }
                     else {
-                        window.mpapi.map = map3D;
-                        window.mpapi.map.setEnabled(true);
-                        switch3D.value = "2D";
+                        loadScript("https://lib.virtualcitymap.de/v3.6.x/lib/Cesium/Cesium.js", function Loaded3DCallback () {
+                            const map3D = abstractAPI.map.createMap(settings3D, "3D");
+
+                            window.mpapi.map = map3D;
+                            window.mpapi.map.setEnabled(true);
+                            switch3D.value = "2D";
+                        });
                     }
                 };
 
