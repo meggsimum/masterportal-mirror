61867d28794f9d3ad1b17332b47643b01837af0f
Tobias Erdrich <te@sinusquadrat.com>
2021-12-06 19:12:08 +0100

    Switched to Bootstrap Icons in vue components, including tests

--- src/modules/tools/wfsSearch/components/WfsSearch.vue
+++ src/modules/tools/wfsSearch/components/WfsSearch.vue
@@ -121,7 +121,7 @@ export default {
     <div>
         <ToolTemplate
             :title="$t(name)"
-            :icon="glyphicon"
+            :icon="icon"
             :active="active"
             :render-to-window="renderToWindow"
             :resizable-window="resizableWindow"
@@ -168,7 +168,7 @@ export default {
                     >
                         <i
                             id="tool-wfsSearch-userHelp-icon"
-                            class="col-md-1 col-sm-1 glyphicon glyphicon-info-sign"
+                            class="col-md-1 col-sm-1 bi-info-circle-fill"
                         />
                         <span
                             id="tool-wfsSearch-userHelp-text"
@@ -224,7 +224,7 @@ export default {
         </ToolTemplate>
         <ModalItem
             :title="$t(name)"
-            :icon="glyphicon"
+            :icon="icon"
             :show-modal="showResults"
             modal-inner-wrapper-style="padding: 10px;min-width: 70vw;"
             modal-content-container-style="padding: 0;overflow: auto;max-height: 70vh;"
