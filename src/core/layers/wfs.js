6c61663c24523913b122a9f12818da6c068391a6
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2022-02-11 12:19:50 +0100

    extends the wfs layer with a parameter for the loading strategy.

--- src/core/layers/wfs.js
+++ src/core/layers/wfs.js
@@ -3,6 +3,7 @@ import LoaderOverlay from "../../utils/loaderOverlay";
 import Layer from "./layer";
 import * as bridge from "./RadioBridge.js";
 import Cluster from "ol/source/Cluster";
+import {bbox, all} from "ol/loadingstrategy.js";
 
 /**
  * Creates a layer of type WMS.
@@ -91,8 +92,10 @@ WFSLayer.prototype.createLayer = function (attrs) {
             },
             loadingParams: {
                 xhrParameters: attrs.isSecured ? {credentials: "include"} : null,
-                propertyname: this.getPropertyname(attrs)
-            }
+                propertyname: this.getPropertyname(attrs),
+                bbox: attrs.bboxGeometry ? attrs.bboxGeometry.getExtent().toString() : undefined
+            },
+            loadingStrategy: attrs.loadingStrategy === "all" ? all : bbox
         };
 
     if (styleFn) {
