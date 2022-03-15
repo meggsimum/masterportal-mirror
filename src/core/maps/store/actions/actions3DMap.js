56175cbda774597b3bf205f77cb5fd10f33d91f5
fmlgv <friedrich.mueller@gv.hamburg.de>
2022-03-15 17:33:35 +0100

    BG-2297:WIP mapMode

--- src/core/maps/store/actions/actions3DMap.js
+++ src/core/maps/store/actions/actions3DMap.js
@@ -31,26 +31,13 @@ function shadowTime () {
     return this.time || Cesium.JulianDate.fromDate(new Date());
 }
 /**
- * Deactivates oblique mode and listens to change event to activate 3d mode.
- * @listens Core#RadioTriggerMapChange
- * @fires Core#RadioTriggerObliqueMapDeactivate
- * @returns {void}
- */
-function deactivateOblique () {
-    Radio.once("Map", "change", function (onceMapMode) {
-        if (onceMapMode === "2D") {
-            activateMap3D();
-        }
-    });
-    Radio.trigger("ObliqueMap", "deactivate");
-}
-/**
  * Reacts if the camera has changed.
  * @fires Core#RadioTriggerMapCameraChanged
  * @returns {void}
  */
 function reactToCameraChanged () {
-    const camera = this.getCamera();
+    // console.log(store.getters["Maps/get3DMap"])
+    const camera = mapCollection.getMap("olcs", "3D").getCamera();
 
     Radio.trigger("Map", "cameraChanged", {"heading": camera.getHeading(), "altitude": camera.getAltitude(), "tilt": camera.getTilt()});
 }
@@ -74,91 +61,15 @@ function createMap3D () {
  * @returns {void}
  */
 function clickEventCallback (clickObject) {
+    store.dispatch("Maps/updateClick", {map3D: clickObject.map3D, position: clickObject.position, pickedPosition: clickObject.pickedPosition, coordinate: clickObject.coordinate, latitude: clickObject.latitude, longitude: clickObject.longitude, resolution: clickObject.resolution, originalEvent: clickObject.originalEvent, map: Radio.request("Map", "getMap")});
     store.dispatch("Map/updateClick", {map3D: clickObject.map3D, position: clickObject.position, pickedPosition: clickObject.pickedPosition, coordinate: clickObject.coordinate, latitude: clickObject.latitude, longitude: clickObject.longitude, resolution: clickObject.resolution, originalEvent: clickObject.originalEvent, map: Radio.request("Map", "getMap")});
     Radio.trigger("Map", "clickedWindowPosition", {position: clickObject.position, pickedPosition: clickObject.pickedPosition, coordinate: clickObject.coordinate, latitude: clickObject.latitude, longitude: clickObject.longitude, resolution: clickObject.resolution, originalEvent: clickObject.originalEvent, map: Radio.request("Map", "getMap")});
 }
 
-/**
- * Activates the map3d if it not already set.
- * If mapmode is "Oblique" it deactivates it.
- * @fires Core#RadioRequestMapGetMapMode
- * @fires Core#RadioTriggerMapBeforeChange
- * @fires Alerting#RadioTriggerAlertAlert
- * @fires Core#RadioTriggerMapChange
- * @returns {void}
- */
-export function activateMap3D () {
-    const mapMode = Radio.request("Map", "getMapMode");
-    let map3D = mapCollection.getMap("olcs", "3D"),
-        scene,
-        camera;
+export default {
+    createMap3D,
+    clickEventCallback,
+    reactToCameraChanged
+};
 
-    if (Radio.request("Map", "isMap3d")) {
-        return;
-    }
-    else if (mapMode === "Oblique") {
-        deactivateOblique();
-        return;
-    }
-    else if (!map3D) {
-        let allLayerModels = Radio.request("ModelList", "getModelsByAttributes", {type: "layer"});
 
-        Radio.trigger("Map", "beforeChange", "3D");
-        allLayerModels = allLayerModels.filter(layerModel => {
-            return ["Oblique", "TileSet3D", "Terrain3D"].indexOf(layerModel.get("typ")) === -1;
-        });
-        allLayerModels.forEach(layerWrapper => {
-            if (layerWrapper.get("isSelected") === false) {
-                layerWrapper.removeLayer();
-            }
-        });
-
-        map3D = createMap3D();
-        mapCollection.addMap(map3D, "olcs", "3D");
-        scene = map3D.getCesiumScene();
-        api.map.olcsMap.prepareScene({scene: scene, map3D: map3D, callback: clickEventCallback}, Config);
-        camera = api.map.olcsMap.prepareCamera(scene, store, map3D, Config, Cesium);
-        camera.changed.addEventListener(reactToCameraChanged.bind(map3D));
-    }
-    map3D.setEnabled(true);
-    Radio.trigger("Map", "change", "3D");
-    store.commit("Map/setMapId", map3D.id);
-    store.commit("Map/setMapMode", "3D");
-    store.dispatch("MapMarker/removePointMarker");
-}
-
-/**
- * Deactivates the 3d mode.
- * @fires Core#RadioRequestMapGetMap
- * @fires Core#RadioTriggerMapBeforeChange
- * @fires Alerting#RadioTriggerAlertAlert
- * @fires Core#RadioTriggerMapChange
- * @returns {void}
- */
-export function deactivateMap3D () {
-    const map3D = mapCollection.getMap("olcs", "3D"),
-        map = Radio.request("Map", "getMap"),
-        view = map.getView();
-    let resolution,
-        resolutions;
-
-    if (map3D) {
-        Radio.trigger("Map", "beforeChange", "2D");
-        view.animate({rotation: 0}, function () {
-            map3D.setEnabled(false);
-            view.setRotation(0);
-            resolution = view.getResolution();
-            resolutions = view.getResolutions();
-            if (resolution > resolutions[0]) {
-                view.setResolution(resolutions[0]);
-            }
-            if (resolution < resolutions[resolutions.length - 1]) {
-                view.setResolution(resolutions[resolutions.length - 1]);
-            }
-            Radio.trigger("Alert", "alert:remove");
-            Radio.trigger("Map", "change", "2D");
-            store.commit("Map/setMapId", map.get("id"));
-            store.commit("Map/setMapMode", "2D");
-        });
-    }
-}
