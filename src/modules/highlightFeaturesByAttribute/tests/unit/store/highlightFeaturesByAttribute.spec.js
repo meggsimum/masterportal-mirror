48c9d89ecb21669b3aa95ae6ffe67ba88689b332
richard_schmid <richard.schmid@gv.hamburg.de>
2022-03-18 14:42:08 +0100

    BG-1977: changed structure and worked on PR comments

--- src/modules/highlightFeaturesByAttribute/tests/unit/store/highlightFeaturesByAttribute.spec.js
+++
@@ -1,6 +0,0 @@
-import {expect} from "chai";
-import sinon from "sinon";
-import actions from "../../../store/actions/highlightFeaturesByAttribute.js";
-
-describe("src/modules/highlightFeaturesByAttribute/store/actions/highlightFeaturesByAttribute.js", () => {
-});
